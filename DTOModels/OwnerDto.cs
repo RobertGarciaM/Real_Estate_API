51c6d738d8c23f925766077c3e9bcd2f4d265e33
Robert Garcia <rhernan.garcia@udea.edu.co>
2023-09-22 09:46:40 -0500

    DTO building

---
+++ DTOModels/OwnerDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOModels
+{
+    public class OwnerDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public byte[] Photo { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ DTOModels/UpdateOwnerDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOModels
+{
+    public class UpdateOwnerDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public IFormFile Photo { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
